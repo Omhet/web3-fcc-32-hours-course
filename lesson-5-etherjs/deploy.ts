600089e02d814047c4842e3edeb03f9dc75cc8a5
Vladimir Ivanov <omhetehmo@yandex.ru>
2022-06-06 12:37:25 +0300

    lesson 5 (ts)

--- lesson-5-etherjs/deploy.ts
+++ lesson-5-etherjs/deploy.ts
@@ -1,10 +1,10 @@
-const ethers = require('ethers')
-const fs = require('fs')
-require('dotenv').config()
+import 'dotenv/config'
+import { ethers } from 'ethers'
+import fs from 'fs'
 
 async function main() {
   const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
-  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, provider)
 
   const abi = fs.readFileSync('./SimpleStorage_sol_SimpleStorage.abi', 'utf8')
   const binary = fs.readFileSync('./SimpleStorage_sol_SimpleStorage.bin', 'utf8')
@@ -14,12 +14,13 @@ async function main() {
   const contract = await contractFactory.deploy()
   console.log('Waiting 1 block to confirm...')
   await contract.deployTransaction.wait(1)
+  console.log('Contract address:', contract.address)
 
   const favoriteNumber = await contract.retreive()
   console.log('Favorite number:', favoriteNumber.toString())
 
   const txResponse = await contract.store('7')
-  const txReceipt = await txResponse.wait(1)
+  await txResponse.wait(1)
 
   const updatedFavoriteNumber = await contract.retreive()
   console.log('Updated favorite number:', updatedFavoriteNumber.toString())
